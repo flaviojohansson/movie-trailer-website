61971bd65e0d9e93bf074d0b1c2895cfaa06508b
Flavio D Johansson <flaviodaniel@outlook.com>
2016-11-06 22:12:38 -0200

    Fix some comments.

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,70 +4,93 @@ import os
 import movie
 import fresh_tomatoes
 
+
 JSON_FILE_NAME = "movies.json"
 DEFAULT_PAGE_TITLE = "Movie Trailer Website"
 
-# Free webapi. For more information, check it out @ omdbapi.com
+# Return JSON from the omdb Free webapi.
 def get_omdb_data(imdb_id):
-    connection = urllib.urlopen("http://www.omdbapi.com/?plot=short&r=json&i=" + imdb_id)
-    output = connection.read()
-    connection.close
-    return output
+    # Any error will throw an exception and funcion exits with no error 
+    try:
+        url = "http://www.omdbapi.com/?plot=short&r=json&i="
+        connection = urllib.urlopen(url + imdb_id)
+        output = connection.read()
+        connection.close
+    except e:
+        output = "{}"  # On exception, create an empty JSON string
 
+    return output
 
 # Return a new Movie object if the data was sucessfully retrieved from omdb API
 def create_movie_object (imdb_id, youtube_trailer_id):
     omdb_data = get_omdb_data(imdb_id)
-    if omdb_data:
-        movies_json = json.loads(omdb_data)
-        new_movie = movie.Movie(
-            title = movies_json["Title"],
-            year = movies_json["Year"],
-            rating = movies_json["Rated"],
-            release_date = movies_json["Released"],
-            runtime = movies_json["Runtime"],
-            genre = movies_json["Genre"],
-            director = movies_json["Director"],
-            writer = movies_json["Writer"],
-            actors = movies_json["Actors"],
-            plot = movies_json["Plot"],
-            language = movies_json["Language"],
-            country = movies_json["Country"],
-            awards = movies_json["Awards"],
-            poster_image_url = movies_json["Poster"],
-            imdb_rating = movies_json["imdbRating"],
-            trailer_youtube_id = youtube_trailer_id            
+
+    # By default this is the returning value
+    new_movie = None
+
+    if omdb_data:  # Was data returned from omdb ?
+        # Avoid malformed JSON
+        try:
+            movies_json = json.loads(omdb_data)
+        except ValueError, e:
+            movies_json = json.loads("{}")  # On exception, create an empty JSON
+
+        # Please Udacity's code reviewers: is this a correct approach
+        # on breaking lines in IF's ? 
+        if "True" in movies_json["Response"] and \
+           "movie" in movies_json["Type"]:  # Must be a movie
+            new_movie = movie.Movie(
+                title = movies_json["Title"],
+                year = movies_json["Year"],
+                rating = movies_json["Rated"],
+                runtime = movies_json["Runtime"],
+                genre = movies_json["Genre"],
+                director = movies_json["Director"],
+                actors = movies_json["Actors"],
+                plot = movies_json["Plot"],
+                poster_image_url = movies_json["Poster"],
+                imdb_rating = movies_json["imdbRating"],
+                trailer_youtube_id = youtube_trailer_id            
         )
-        return new_movie
 
-    # When no data is get from omdb site, returns None
-    return None
-
+    return new_movie
 
 # Main function just to keep 'movie' names concerns separated 
 def main():
     # Create an empty movies list
     movies = []
+
     # Default page title in case none is supplied in the JSON file
     page_title = DEFAULT_PAGE_TITLE
-    # Open the json file if it exists and reads it
-    dir_path = os.path.dirname(os.path.realpath(__file__))  # Directory of this file
+
+    # Build full path for JSON file
+    dir_path = os.path.dirname(os.path.realpath(__file__))  # Directory of this py file
     full_path = "\\".join([dir_path, JSON_FILE_NAME])
-    if os.path.isfile(full_path):
+
+    if os.path.isfile(full_path):  # Check if file exists
+        # Open JSON file and parse the data to a JSON object
         movies_json_data = open(full_path).read()
-        # Parse the data to a JSON object
-        movies_json = json.loads(movies_json_data)
+
+        # Avoid malformed JSON
+        try:
+            movies_json = json.loads(movies_json_data)
+        except ValueError, e:
+            movies_json = json.loads("{}")  # On exception, create an empty JSON
+
         # Set page name if key exists in JSON file
         if ("page_title" in movies_json):
             page_title = movies_json["page_title"]
-        # Check it key movies exists in JSON file
-        if ("movies" in movies_json):            
-            # For each movie, retrieve information from omdbAPI  
+
+        if ("movies" in movies_json):  # Check if key 'movies' exists in JSON file
+            # For each movie, try to create a Movie() object and add to movies List
             for movie in movies_json["movies"]:
+                # Create an instance of Movie() on success, or None
                 movie_instance = create_movie_object(movie["imdb_id"],
                                                      movie["youtube_trailer_id"])
+                # Add the new Movie() object to movies List
                 if movie_instance:
                     movies.append(movie_instance)
+
     # Finally render the webpage an open it in webbrowser
     fresh_tomatoes.open_movies_page(page_title, movies)
 
--- movie.py
+++ movie.py
@@ -2,26 +2,36 @@ import webbrowser
 
 
 class Movie():
-    """ This class provides information  for the movies being displayed """
+    """This class provides information for the movies being displayed
     
+    Attributes:
+        title (str): Title of the movie
+        year (str): Release Year
+        rating (str): One of the VALID_RATINGS
+        runtime (str): Duration of the movie
+        genre (str): Free text describing the genre
+        director (str): Name of director
+        actors (str): Name of actors 
+        plot (str): Short description of the movie plot 
+        poster_image_url (str): Full URL to the movie poster image
+        imdb_rating (str): Grade of the movie
+        trailer_youtube_id (str): Youtube's movie traiiler id 
+
+    """
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, title, year, rating, release_date, runtime, genre, director, 
-                 writer, actors, plot, language, country, awards, poster_image_url,
-                 imdb_rating, trailer_youtube_id):
+    def __init__(self, title, year, rating, runtime, genre, director, 
+                 actors, plot, poster_image_url, imdb_rating,
+                 trailer_youtube_id):
         self.title = title
         self.year = year
         self.rating = rating
-        self.release_date = release_date
         self.runtime = runtime
         self.genre = genre
         self.director = director
-        self.writer = writer
         self.actors = actors
         self.plot = plot
-        self.language = language
-        self.country = country
-        self.awards = awards
         self.poster_image_url = poster_image_url
         self.imdb_rating = imdb_rating
         self.trailer_youtube_id = trailer_youtube_id
